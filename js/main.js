1d524276b4790975dbfaf9d98bdb920a62ece41e
lararb <lararb91@gmail.com>
2019-04-13 23:12:35 +0200

    finish step 4

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 'use strict';
 
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
 
 function ten(word, number) {
     for (let i=0; i<number; i++) {
     console.log(word,);
     }
 }
-ten('Patata', 10);
-ten('Aguacate', 7);
-ten('Pizza', 50);
+ten('Patata', getRandomNumber(10));
+ten('Aguacate', getRandomNumber(10));
+ten('Pizza', getRandomNumber(10));
