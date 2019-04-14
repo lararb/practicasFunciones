e6ea013ff59cb2059634b2d8d8c9fde6e96cedd2
lararb <lararb91@gmail.com>
2019-04-14 18:09:25 +0200

    write names of array

--- js/main.js
+++ js/main.js
@@ -1,14 +1,46 @@
 'use strict';
 
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+const myWordList =  [
+  {
+    text: 'Pencil',
+    total: 6
+  },
+  {
+    text: 'Thermo',
+    total: 2
+  },
+  {
+    text: 'TV',
+    total: 8
+  },
+  {
+    text: 'Phone',
+    total: 4
   }
+];
 
-function ten(word, number) {
-    for (let i=0; i<number; i++) {
-    console.log(word,);
-    }
+function writeMyArray(myWordList) {
+  for (const array of myWordList) {
+    console.log(array.text); 
+  }
 }
-ten('Patata', getRandomNumber(10));
-ten('Aguacate', getRandomNumber(10));
-ten('Pizza', getRandomNumber(10));
+
+writeMyArray(myWordList);
+
+
+// function getRandomNumber(max) {
+//     return Math.ceil(Math.random() * max);
+//   }
+
+//  function ten(word, number) {
+//    for (let i=0; i<number; i++) {
+//    console.log(word);
+//     }
+// }
+// ten('Patata', getRandomNumber(10));
+// ten('Aguacate', getRandomNumber(10));
+// ten('Pizza', getRandomNumber(10));
+
+
+
+
